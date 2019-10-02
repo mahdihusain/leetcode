80c4668b57dad9dbb3cf2d36c87e429aacb66838
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2019-10-02 18:24:11 +0530

    Create [EASY] Unique Number of Occurrences.java

---
+++ [EASY] Unique Number of Occurrences.java
@@ -0,0 +1,35 @@
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> ocMap = new HashMap();
+        for(int i=0; i<arr.length; i++){
+            int currOc = ocMap.getOrDefault(arr[i], 0);
+            ocMap.put(arr[i], currOc+1);
+        }
+        Set<Integer> hSet = new HashSet(ocMap.values());
+        return hSet.size()==ocMap.size();
+    }
+}
