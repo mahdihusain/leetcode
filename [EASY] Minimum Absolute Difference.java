85e0c8babdaf8e0197c2203bf30d47cb5da3f260
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2019-10-02 18:16:07 +0530

    Update [EASY] Minimum Absolute Difference.java

--- [EASY] Minimum Absolute Difference.java
+++ [EASY] Minimum Absolute Difference.java
@@ -20,6 +20,12 @@ Example 3:
 
 Input: arr = [3,8,-10,23,19,-4,-14,27]
 Output: [[-14,-10],[19,23],[23,27]]
+ 
+
+Constraints:
+
+2 <= arr.length <= 10^5
+-10^6 <= arr[i] <= 10^6
 
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
