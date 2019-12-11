5e50dc908be79f2ca49f286f52cb38ba961a4a33
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2019-12-11 18:11:36 +0530

    Create [MEDIUM] Group the People Given the Group Size They Belong To.java

---
+++ [MEDIUM] Group the People Given the Group Size They Belong To.java
@@ -0,0 +1,39 @@
+There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group. Given the array groupSizes of length n telling the group size each person belongs to, return the groups there are and the people's IDs each group includes.
+
+You can return any solution in any order and the same applies for IDs. Also, it is guaranteed that there exists at least one solution. 
+
+ 
+
+Example 1:
+
+Input: groupSizes = [3,3,3,3,3,1,3]
+Output: [[5],[0,1,2],[3,4,6]]
+Explanation: 
+Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]].
+Example 2:
+
+Input: groupSizes = [2,1,3,3,3,2]
+Output: [[1],[0,5],[2,3,4]]
+ 
+
+Constraints:
+
+groupSizes.length == n
+1 <= n <= 500
+1 <= groupSizes[i] <= n
+
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        for(int i=0; i<groupSizes.length; i++) {
+            List<Integer> val = map.computeIfAbsent(groupSizes[i], k -> new LinkedList<Integer>());
+            val.add(i);
+            if(val.size()==groupSizes[i]) {
+                res.add(val);
+                map.put(groupSizes[i], new LinkedList<Integer>());
+            }
+        }
+        return res;
+    }
+}
