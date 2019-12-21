0b80d8485dbd812407caa00fec4366b58a3e68db
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2019-12-21 19:23:52 +0530

    Create [MEDIUM] Remove Zero Sum Consecutive Nodes from Linked List.java

---
+++ [MEDIUM] Remove Zero Sum Consecutive Nodes from Linked List.java
@@ -0,0 +1,54 @@
+Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+
+After doing so, return the head of the final linked list.  You may return any such answer.
+
+ 
+
+(Note that in the examples below, all sequences are serializations of ListNode objects.)
+
+Example 1:
+
+Input: head = [1,2,-3,3,1]
+Output: [3,1]
+Note: The answer [1,2,1] would also be accepted.
+Example 2:
+
+Input: head = [1,2,3,-3,4]
+Output: [1,2,4]
+Example 3:
+
+Input: head = [1,2,3,-3,-2]
+Output: [1]
+ 
+
+Constraints:
+
+The given linked list will contain between 1 and 1000 nodes.
+Each node in the linked list has -1000 <= node.val <= 1000.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        Map<Integer, ListNode> prefixMap = new HashMap<Integer, ListNode>();
+        int prefix = 0;
+        for(ListNode i=dummy; i!=null; i=i.next) {
+            prefix += i.val;
+            prefixMap.put(prefix, i);
+        }
+        prefix = 0;
+        for(ListNode i=dummy; i!=null; i=i.next) {
+            prefix += i.val;
+            i.next = prefixMap.get(prefix).next;
+        }
+        return dummy.next;
+    }
+}
